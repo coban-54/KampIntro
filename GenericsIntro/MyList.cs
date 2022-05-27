5f36d2f119a03803dd9c1bccdcf2efd7522f906f
coban-54 <92750818+coban-54@users.noreply.github.com>
2022-05-28 00:58:44 +0300

    26.05.2022 Kodlama io ders notu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    internal class MyList<T>
+    {
+        T[] İtems;//1 dizim var dizimi ekleyeceğim.
+        //constractor
+        public MyList()   //ctor tab tab listeyi burda newliyoruz
+        {
+            İtems = new T[0];// sıfır elemanlı dizi
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = İtems;// geçici dizinin refi item in refi
+            İtems = new T[İtems.Length+1];//Dizimin elaman sayısını bir arttırıyoruz
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                İtems[i] = tempArray[i];
+            }
+            İtems[İtems.Length - 1] = item;//üstte temp a. items a geçerken +1 index fazlalık kalır
+            // o sebepten  -1  ini  alırız null siler
+
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericsIntro // Genericler T =Type a göre ekler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Engin");
+            
+        }
+    }
+}
